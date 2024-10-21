1fa009375566fd5caf6a8e559393221e0be43c38
Natnael Birhan Kidane <157119095+Natnaelbr@users.noreply.github.com>
2024-10-21 10:43:45 +0300

    controllers

---
+++ file.controller.js
@@ -0,0 +1,48 @@
+const db = require("../models");
+const File = db.files;
+const path = require('path');
+
+// Upload file to MySQL
+const uploadFile = async (req, res) => {
+  try {
+    const { originalname, mimetype, buffer } = req.file;
+
+    await File.create({
+      fileName: originalname,
+      fileType: mimetype,
+      fileData: buffer,
+    });
+
+    res.status(200).send("File uploaded successfully.");
+  } catch (err) {
+    res.status(500).send({
+      message: "Could not upload the file: " + err.message,
+    });
+  }
+};
+
+// Download file from MySQL
+const downloadFile = async (req, res) => {
+  try {
+    const file = await File.findByPk(req.params.id);
+
+    if (!file) {
+      return res.status(404).send({
+        message: "File not found."
+      });
+    }
+
+    res.setHeader('Content-Disposition', `attachment; filename=${file.fileName}`);
+    res.setHeader('Content-Type', file.fileType);
+    res.send(file.fileData);
+  } catch (err) {
+    res.status(500).send({
+      message: "Could not download the file: " + err.message,
+    });
+  }
+};
+
+module.exports = {
+  uploadFile,
+  downloadFile,
+};
